5e745f4b9e2d57a3d88c8d5e39de3904d74f03c5
Philip Levy <philipdlevy@gmail.com>
2022-03-01 14:56:03 -0700

    commit tree 78a83c3d5390cbbe5312d760b3231c9ade4c4531

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-// Write your code here!
+// Write your code here 
+main.remove()
 
-main.remove();
+const newHeader = document.createElement('h1');
 
-const newHeader = document.createElement("h1");
-newHeader.id = 'victory';
-newHeader.textContent = "Philip is the champion";
+//newHeader.setAttribute("id", "victory");
 
-document.body.append(newHeader);
+newHeader.id = "victory"
 
-
-
+newHeader.textContent = "Philip is the champion"
 
 
 
@@ -21,42 +19,3 @@ document.body.append(newHeader);
 
 
 
-
-/*
-const element = document.createElement("div");
-
-document.body.append(element);
-
-const ul = document.createElement("ul");
-
-for (let i = 0; i < 3; i++) {
-  const li = document.createElement("li");
-  li.textContent = (i + 1).toString();
-  ul.append(li);
-}
-
-element.append(ul);
-*/
-
-
-
-
-
-
-
-/*
-element.textContent = "You've changed what's on the screen!";
-
-element.style.fontSize = "24px";
-element.style.marginLeft = "30px";
-element.style.lineHeight = 2;
-
-element.className = "pet-listing dog";
-
-<main id="main" class="pet-listing dog"></main>
-
-element.classList.remove("dog");
-element.classList.add("cat", "sale");
-
-<main id="main" class="pet-listing cat sale"></main>
-*/
